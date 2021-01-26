6ad119a2f2840f9436f48d859580103a5903913c
yangxunwu <yangxunwu@outlook.com>
2021-01-25 23:34:41 -0500

    添加termios终端配置说明及vt100终端码

--- src/more/more.cpp
+++ src/more/more.cpp
@@ -42,15 +42,42 @@ void displayContent(std::istream& is)
     }
 }
 
+/**
+    \033[0m		// 关闭所有属性
+    \033[1m		// 设置为高亮
+    \033[4m		// 下划线
+    \033[5m		// 闪烁
+    \033[7m		// 反显
+    \033[8m		// 消隐
+    \033[nA		// 光标上移 n 行
+    \033[nB		// 光标下移 n 行
+    \033[nC		// 光标右移 n 行
+    \033[nD		// 光标左移 n 行
+    \033[y;xH	// 设置光标位置
+    \033[2J		// 清屏
+    \033[K		// 清除从光标到行尾的内容
+    \033[s		// 保存光标位置
+    \033[u		// 恢复光标位置
+    \033[?25l	// 隐藏光标
+    \033[?25h	// 显示光标
+
+ **/
 int displayMore() 
 {
-    //终端无回现并直接读取输入无需回车
     static struct termios oldT, newT;
     tcgetattr(STDIN_FILENO, &oldT);
     newT = oldT;
+
+    //设置非标准模式
     newT.c_lflag &= ~(ICANON);
+
+    //不回显输入
     newT.c_lflag &= ~(ECHO);
+
+    //设置最少的读入数据量,当有一个字节数据的时候即可返回
     newT.c_cc[VMIN] = 1;
+
+    //等待读入超时时间,读到数据后立刻返回
     newT.c_cc[VTIME] = 0;
     tcsetattr(STDIN_FILENO, TCSANOW, &newT);
 
@@ -60,6 +87,7 @@ int displayMore()
 
     while(std::cin.get(reply)) {
 
+        //清空当前行
         std::cout << "\033[7D";
         std::cout << "\033[K";
         std::cout << "\033[u";
