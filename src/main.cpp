3f95674cb410862d88bd492eaa17d25833966b4a
yangxunwu <yangxunwu@outlook.com>
2021-02-06 10:01:29 -0500

    curses api终端打印

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,23 @@
 #include <iostream>
-#include <signal.h>
+#include <curses.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <time.h>
-#include <string.h>
-
-/**
- * 处理signal信号
- **/
-void signalHandler(int signal) {
-    std::cout << "signalHandler get signlal " << signal << std::endl;
-}
 
 int main(int argc, char* argv[]) {
 
-    /**
-     * kill -9
-     **/
-    signal(SIGKILL, signalHandler); 
+    initscr();
+    clear();
     
-    /**
-     * ctrl + z
-     **/
-    signal(SIGSTOP, signalHandler);
-
-    
-    int fd = open("/home/course/unix/output.data", O_RDWR | O_TRUNC | O_CREAT);
-    if(fd == -1) {
-        std::perror("/home/course/unix/output.data");
-        exit(1);
-    }
-    while(true) {
+    for(int i = 0; i < LINES; i++) {
+        move(i, i + 1);
+        if(i % 2 == 1) {
+            standout();
+        }
+        addstr("Hello,World");
+        if(i % 2 == 1) {
+            standend();
+        }
         sleep(1);
-        write(fd, "abc\n", 4);
+        refresh();  
     }
+    endwin();
 }
